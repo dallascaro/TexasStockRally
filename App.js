3233eb982eaac2e7f5e1d95bbbde598d73c645dd
Dallas Caro <56410151+dallascaro@users.noreply.github.com>
2024-09-08 21:51:22 -0500

    Starting the first page

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style= {styles.header}>
+        <Text>Texas Stock Rally</Text>
+      </View>
+      <View style= {styles.authentication}>
+          <View style={styles.email}>
+            <Text>Email</Text>
+            <TextInput
+            style={styles.emailInput}
+            placeholder='enter email'>
+            </TextInput>
+          </View>
+          <View style={styles.password}>
+            <Text>Password</Text>
+            <TextInput
+            style={styles.passwordInput}
+            placeholder='password'
+            secureTextEntry>
+            </TextInput>
+          </View>
+          <View>
+          <TouchableOpacity>
+            <Text>Forgot password</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.buttons}>
+        <TouchableOpacity style={styles.login}>
+          <Text>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.signup}>
+          <Text>Sign up</Text>
+        </TouchableOpacity>
+        </View>
+        </View>
+      </View>
   );
 }
 
@@ -17,4 +47,38 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  title: {
+
+  },
+  authentication:{
+    flex: 1
+  },
+  email:{
+    flexDirection: 'row'
+  },
+  emailInput: {
+    textAlign: 'center',
+  },
+  password: {
+    flexDirection: 'row'
+  },
+  passwordInput:{
+  textAlign: 'center'
+  },
+  buttons:{
+    flex: 1,
+    flexDirection: 'row'
+  },
+  login:{
+    margin: 10
+  },
+  signup: {
+    margin: 10
+  },
+
 });
